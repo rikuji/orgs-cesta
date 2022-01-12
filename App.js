5adf53c2af10b96c9d53477d5605f742ca148ec2
Lucas Valenzuela <lucasbezerra1701@gmail.com>
2022-01-12 16:29:08 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,25 @@
+import { StatusBar, SafeAreaView, View } from 'react-native'
+import {
+  useFonts,
+  Montserrat_400Regular,
+  Montserrat_700Bold,
+} from '@expo-google-fonts/montserrat'
+
+import Cesta from './src/telas/Cesta'
+import mock from './src/mocks/cesta'
+
+export default function App() {
+  let [fonteCarregada] = useFonts({
+    MontserratRegular: Montserrat_400Regular,
+    MontserratBold: Montserrat_700Bold,
+  })
+
+  if (!fonteCarregada) return <View />
+
+  return (
+    <SafeAreaView>
+      <StatusBar />
+      <Cesta {...mock} />
+    </SafeAreaView>
+  )
+}
